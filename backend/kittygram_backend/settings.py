1fa78032812ef100a31bee1fc0d419d897abc481
Ilya-Reznikov60 <reznikov.iluxa@yandex.ru>
2023-08-25 05:55:45 +0000

    fix comments after first review

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['51.250.31.101', '127.0.0.1', 'localhost', 'kittygramwork.site']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
